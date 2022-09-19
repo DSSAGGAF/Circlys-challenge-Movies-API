8cc6732e487fade7e4b35caa15fb8db44cfab1a3
Abuda7m <dssaggaf@gmail.com>
2022-09-20 03:55:12 +0800

    Each Movie Page has available time slots.

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const Model = require('../models/model');
 const router = express.Router();
 
-//Post Method
-router.post('/post', async (req, res) => {
+//create movie
+router.post('/create', async (req, res) => {
     const data = new Model({
         name: req.body.name,
         showtimes: req.body.showtimes
@@ -18,10 +18,10 @@ router.post('/post', async (req, res) => {
     }
 })
 
-//Get all Method
+//Get all movies
 router.get('/get_movies', async (req, res) => {
     try {
-        const data = await Model.find({}, { _id: 0, name: 1,});
+        const data = await Model.find({}, { _id: 1, name: 1,});
         res.json(data)
     }
     catch (error) {
@@ -29,5 +29,45 @@ router.get('/get_movies', async (req, res) => {
     }
 })
 
+//Get movie by ID
+router.get('/get_movie/:id', async (req, res) => {
+    try {
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//Update movie by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//Delete movie by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
 
 module.exports = router;
