69e6c017d2f5111071ef311a3570062df49f6a69
Abuda7m <dssaggaf@gmail.com>
2022-09-20 04:32:21 +0800

    Users can reserve a time slot and book it for one or more people.

--- routes/routes.js
+++ routes/routes.js
@@ -21,7 +21,7 @@ router.post('/create', async (req, res) => {
 //Get all movies
 router.get('/get_movies', async (req, res) => {
     try {
-        const data = await Model.find({}, { _id: 1, name: 1,});
+        const data = await Model.find({}, { _id: 1, name: 1, });
         res.json(data)
     }
     catch (error) {
@@ -40,18 +40,30 @@ router.get('/get_movie/:id', async (req, res) => {
     }
 })
 
-//Update movie by ID Method
-router.patch('/update/:id', async (req, res) => {
+//book movie by ID Method
+router.patch('/book/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        const updatedData = req.body;
+        const numOfTickets = req.body.number_of_tickets;
+        const slotId = req.body.slot_id;
+        const arrayIndex = slotId - 1;
         const options = { new: true };
 
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(result)
+        const movie = await Model.findById(id)
+        if (slotId > 6 || slotId < 1) {
+            res.send("Slot id is wrong")
+        }
+        else {
+            movieShowtimes = movie.showtimes[arrayIndex]
+            if (movieShowtimes.capacity < numOfTickets) {
+                res.send(`Only ${movieShowtimes.capacity} seats avilable!!`)
+            }
+            else {
+                movie.showtimes[arrayIndex].capacity = movieShowtimes.capacity - numOfTickets;
+                const result = await Model.findByIdAndUpdate(movie.id, movie, options)
+                res.send(result)
+            }
+        }
     }
     catch (error) {
         res.status(500).json({ message: error.message })
@@ -63,7 +75,7 @@ router.delete('/delete/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
+        res.send(`Movie with ${data.name} has been deleted..`)
     }
     catch (error) {
         res.status(400).json({ message: error.message })
